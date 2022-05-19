1be2a41cb34857dfa11416516b641ad4052885e1
Adadezer <adadezer@gmail.com>
2022-05-18 22:56:42 -0300

    adicionar tarefas

---
+++ app/frontend/src/App.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
+import './App.css';
+
+
+
+const App = () => {
+  const  [tasks, setTasks] = useState([
+    {
+      id: '1',
+      title: 'Estudar Programação',
+      completed: false,
+    },
+    {
+      id: '2',
+      title: 'Ler Livros',
+      completed: true,
+    },
+  ]);
+
+  const handleTaskAddition = (taskTitle) => {
+    const newTask = [...tasks,{
+        title: taskTitle,
+        id: Math.random(10),
+        completed: false,
+      }
+    ];
+    setTasks(newTask);
+  }
+  
+  return (
+    <>
+      <div className='container'>
+        <AddTask handleTaskAddition={handleTaskAddition}/>
+        <Tasks tasks={ tasks }/>
+      </div>
+    </>
+  )
+}
+
+export default App;
