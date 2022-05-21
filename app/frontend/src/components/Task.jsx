73e404d921a927eddbd3e7210c23d515377537b1
Adadezer <adadezer@gmail.com>
2022-05-21 16:21:18 -0300

    fix: numero da linha, e status

--- app/frontend/src/components/Task.jsx
+++ app/frontend/src/components/Task.jsx
@@ -2,20 +2,26 @@ import React from 'react';
 
 import './css/Task.css';
 
-const Task = ({task}) => {
+const Task = ({task, numLinha}) => {
   return (
     <div className='task-container'>
-      <div className='task-id'>
-        {task.id}
+      <div className={numLinha < 9 ? 'task-numLinha': 'task-numLinha10'}>
+        {numLinha + 1}
       </div>
       <div className='task-descricao'>
         {task.descricao}
       </div>
-      <div className='task-status'>
+      <div 
+        className={
+          task.status === 'concluido' ? 'task-status-con'
+            : task.status === 'em andamento' ? 'task-status-and'
+            : 'task-status-pen'
+        }
+      >
         {task.status}
       </div>
       <div className='task-createdAt'>
-        {task.createdAt}
+        {task.createdAt.split('T', 1)}
       </div>
     </div>
   );
--- app/frontend/src/components/Tasks.jsx
+++ app/frontend/src/components/Tasks.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import Task from  './Task'
 
 const Tasks = ({tasks}) => {
-  
   return (
     <>
-      {tasks.map((task) => (
+      {tasks.map((task, indice) => (
         <Task
           key={task.id}
           task={task}
+          numLinha={indice}
         />
       ))}
     </>
